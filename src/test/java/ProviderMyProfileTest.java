fdf36a65f450694f549cc945695dfad7fc683b8a
MIHIRSHARMA2000 <mihirsharma@deloitte.com>
2022-04-28 16:09:43 +0530

    Added exceptional handling and test cases

--- src/test/java/ProviderMyProfileTest.java
+++ src/test/java/ProviderMyProfileTest.java
@@ -1,9 +1,7 @@
-import Pages.Baseclass;
-import Pages.ProviderMyProfile;
-import Pages.ProviderSignIn;
-import Pages.ProviderSignUp;
+import Pages.*;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -11,7 +9,6 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static Pages.Baseclass.driver;
-
 public class ProviderMyProfileTest {
     public ProviderMyProfileTest() throws IOException {
         super();
@@ -32,20 +29,12 @@ public class ProviderMyProfileTest {
         ProviderSignIn.addEmail();
         ProviderSignIn.addPassword();
         ProviderSignIn.clickSignIn();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
         ProviderMyProfileObj.profile_pic();
         ProviderMyProfileObj.Click_on_myProfile();
-        //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
         Thread.sleep(3000);
         ProviderMyProfileObj.jsClick(driver);
-        /*ProviderMyProfileObj.valid_first_name();
-        ProviderMyProfileObj.valid_last_name();
-        ProviderMyProfileObj.address();
-        ProviderMyProfileObj.getAddress2();
-        ProviderMyProfileObj.validPincode();
-        ProviderMyProfileObj.valid_district();
-        ProviderMyProfileObj.validState();
-        ProviderMyProfileObj.validCountry();*/
+
         ProviderMyProfileObj.js1Click(driver);
     }
     @AfterTest
